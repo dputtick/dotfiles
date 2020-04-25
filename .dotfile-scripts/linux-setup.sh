96ef23530b1464691e24510c81871bb71bc546d7
Dan Puttick <dputtick@users.noreply.github.com>
2020-04-25 16:40:34 -0400

    Linux setup script

--- .dotfile-scripts/linux-setup.sh
+++ .dotfile-scripts/linux-setup.sh
@@ -1,2 +1,17 @@
 sudo apt update bash
-sudo apt install tmux vim mosh htop curl
+# [ ] Install Firefox
+# [ ] Use Mozilla sync to sync extensions
+# [ ] Generate a new set of ssh keys: `ssh-keygen -t ed25519` + all default settings
+# [ ] Add ssh public key to Github via the web interface
+# [ ] sudo apt install git
+# [ ] Follow instructions from README in dotfiles repo to install
+# [ ] Run this script
+# [ ] Install Sublime Text via the Sublime website
+# [ ] Install Package Control and restart
+# [ ] Copy the contents of the Sublime extensions User directory from dotfiles to the correct location
+# [ ] Install rbenv, pyenv, and nvm via their respective install scripts
+# [ ] Remap caps lock to ctrl in gnome-tweak
+# [ ] Install Dropbox from website
+# [ ] Install Rescuetime from website
+
+sudo apt install tmux vim mosh htop curl gnome-tweak-tool fonts-inconsolata xclip
