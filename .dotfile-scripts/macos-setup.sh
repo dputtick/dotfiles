5939118c6743d205f806da5ef439e712a05fbf54
Dan Puttick <dan@relevant.healthcare>
2020-05-31 18:19:39 -0400

    Fix scripts and platform-conditional sourcing

--- .dotfile-scripts/macos-setup.sh
+++ .dotfile-scripts/macos-setup.sh
@@ -1,17 +1,16 @@
-sudo apt update bash
 # [ ] Install Firefox
 # [ ] Use Mozilla sync to sync extensions or install them manually
 # [ ] Generate a new set of ssh keys: `ssh-keygen -t ed25519` + all default settings
 # [ ] Add ssh public key to Github via the web interface
-# [ ] sudo apt install git
 # [ ] Follow instructions from .dotfile-README.md in dotfiles repo to install dotfiles
 # [ ] Run this script
-# [ ] Install Sublime Text via the Sublime website
-# [ ] Install Package Control and restart
+# [ ] Open Sublime, install Package Control and restart
 # [ ] Copy the contents of the Sublime extensions User directory from dotfiles to the correct location
 # [ ] Install rbenv, pyenv, and nvm via their respective install scripts
-# [ ] Remap caps lock to ctrl in gnome-tweak
 # [ ] Install Dropbox from website
 # [ ] Install Rescuetime from website
 
-sudo apt install tmux vim mosh htop curl gnome-tweak-tool fonts-inconsolata xclip
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install fish tmux mosh htop curl rbenv git bash ripgrep
+brew cask install sublime-text
