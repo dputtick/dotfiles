5d73b55f9a80ff7582c32c98a88bf0dd5166cbc7
Dan Puttick <dan@relevant.healthcare>
2020-06-14 18:49:03 -0400

    Add support for pyenv to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,11 @@
 set --universal fish_user_paths $fish_user_paths ~/.rbenv/shims
 set --universal fish_user_paths $fish_user_paths /usr/local/opt/postgresql@9.6/bin
+set --universal PYENV_ROOT $HOME/.pyenv
+set --universal fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+
+if command -v pyenv 1>/dev/null 2>&1
+    pyenv init - | source
+end
 
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
